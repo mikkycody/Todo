9ee1ce223b8af479fde005afbed073e1be916de2
Mikkycody <horluwatowbeey@gmail.com>
2020-07-08 11:56:08 +0100

    Refactoring

---
+++ todosCOntrollers/todos.js
@@ -0,0 +1,123 @@
+import db from '../db/db.js';
+
+class TodosController{
+    getAllTodos(req, res){
+        res.status(200).send({
+            success : 'true',
+            message : 'todos retrieved successfully',
+            todos   : db
+        })
+    };
+    
+    createTodo(req, res){
+        if(!req.body.title){
+            return res.status(400).send({
+                success : 'false',
+                message: 'Title is required'
+            })
+        } else if(!req.body.description){
+            return res.status(400).send({
+                success : 'false',
+                message: 'Description is required'
+            })
+        }
+    
+        const todo = {
+            id : db.length +1,
+            title: req.body.title,
+            description: req.body.description
+        };
+    
+        db.push(todo);
+        return res.status(201).send({
+            success : "true",
+            message : 'Todo has been added'
+        })
+    
+    };
+    
+    getSingleTodo(req, res) {
+        const id = parseInt(req.params.id, 10);
+    
+        db.map((todo) =>{
+            if(todo.id == id){
+                res.status(200).send({
+                    success: true,
+                    message: "Todo retreieved successfully",
+                    todo
+                })
+            }
+        })
+        res.status(404).send({
+            success: false,
+            message: "Todo does not exist",
+        })
+    };
+    
+    deleteTodo(req, res){
+        const id = parseInt(req.params.id, 10);
+        db.map((todo, index) =>{
+            if(todo.id == id){
+                db.splice(index,1)
+                res.status(200).send({
+                    success:true,
+                    message: "Todo deleted successfully",
+                    todo
+                })
+            }
+        })
+        res.status(404).send({
+            success:false,
+            message: "Todo does not exist",
+        })
+    };
+    
+    updateTodo(req, res)  {
+        const id = parseInt(req.params.id, 10);
+        let todoFound;
+        let itemIndex;
+        db.map((todo, index) => {
+          if (todo.id === id) {
+            todoFound = todo;
+            itemIndex = index;
+          }
+        });
+      
+        if (!todoFound) {
+          return res.status(404).send({
+            success: 'false',
+            message: 'todo not found',
+          });
+        }
+      
+        if (!req.body.title) {
+          return res.status(400).send({
+            success: 'false',
+            message: 'title is required',
+          });
+        } else if (!req.body.description) {
+          return res.status(400).send({
+            success: 'false',
+            message: 'description is required',
+          });
+        }
+      
+        const updatedTodo = {
+          id: todoFound.id,
+          title: req.body.title || todoFound.title,
+          description: req.body.description || todoFound.description,
+        };
+      
+        db.splice(itemIndex, 1, updatedTodo);
+      
+        return res.status(201).send({
+          success: 'true',
+          message: 'todo added successfully',
+          updatedTodo,
+        });
+      };
+    
+}
+
+const TodoController = new TodosController();
+export default TodoController;
